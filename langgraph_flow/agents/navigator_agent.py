c564e356bf1db8f50cf556943cdb514204654912
manavgarkel <manav.garkel@gmail.com>
2025-07-28 18:04:10 -0400

    renamed constants

--- langgraph_flow/agents/navigator_agent.py
+++ langgraph_flow/agents/navigator_agent.py
@@ -2,14 +2,14 @@ from typing import Dict
 
 from langgraph_flow.agents.agent import Agent
 from langgraph_flow.models.assistant_state import AssistantState
-from utils.constants import DEFAULT_TOP_K_NAVIGATOR, KEY_CONFIG_NAVIGATOR
+from utils.constants import DEFAULT_TOP_K_NAVIGATOR, KEY_NAVIGATOR
 
 NAVIGATOR_PROMPT_TEMPLATE_TEXT = "navigation_prompt.txt"
 
 
 def navigate_code(state: AssistantState) -> Dict:
     agent = Agent(
-        agent_type=KEY_CONFIG_NAVIGATOR,
+        agent_type=KEY_NAVIGATOR,
         prompt_file=NAVIGATOR_PROMPT_TEMPLATE_TEXT,
         default_top_k=DEFAULT_TOP_K_NAVIGATOR,
         is_input_code=True,
