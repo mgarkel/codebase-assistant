88c4bec2ddb6b99ab20e9a20b614789f98cf440a
manavgarkel <manav.garkel@gmail.com>
2025-07-29 18:02:26 -0400

    removed unused faiss code

--- ingestion/load_vectorstore.py
+++ ingestion/load_vectorstore.py
@@ -3,16 +3,9 @@ import os
 from typing import Dict
 
 from langchain_chroma import Chroma
-from langchain_community.vectorstores import FAISS
 
 from langgraph_flow.models.openai_model import OpenAIModel
-from utils.constants import (
-    KEY_CHROMA,
-    KEY_FAISS,
-    KEY_PERSIST_DIRECTORY,
-    KEY_TYPE,
-    KEY_VECTORSTORE,
-)
+from utils.constants import KEY_PERSIST_DIRECTORY, KEY_VECTORSTORE
 
 logger = logging.getLogger(__name__)
 
@@ -26,31 +19,16 @@ def load_vectorstore(cfg: Dict):
         return _VECTORSTORE
 
     store_cfg = cfg.get(KEY_VECTORSTORE, {})
-    store_type = store_cfg.get(KEY_TYPE, KEY_CHROMA).lower()
     persist_dir = store_cfg.get(
         KEY_PERSIST_DIRECTORY, "vectorstore/chroma_index/"
     )
     os.makedirs(persist_dir, exist_ok=True)
-
     embeddings = OpenAIModel(cfg).embedding_model
-
     try:
-        if store_type == KEY_CHROMA:
-            logger.info("Loading Chroma vectorstore from '%s'", persist_dir)
-            _VECTORSTORE = Chroma(
-                persist_directory=persist_dir, embedding_function=embeddings
-            )
-        elif store_type == KEY_FAISS:
-            index_path = os.path.join(persist_dir, "faiss_index")
-            logger.info("Loading FAISS vectorstore from '%s'", index_path)
-            _VECTORSTORE = FAISS.load_local(
-                folder_path=index_path, embedding=embeddings
-            )
-        else:
-            msg = f"Unsupported vectorstore type: '{store_type}'"
-            logger.error(msg)
-            raise ValueError(msg)
-
+        logger.info("Loading Chroma vectorstore from '%s'", persist_dir)
+        _VECTORSTORE = Chroma(
+            persist_directory=persist_dir, embedding_function=embeddings
+        )
         logger.info("Vectorstore loaded successfully")
         return _VECTORSTORE
 
